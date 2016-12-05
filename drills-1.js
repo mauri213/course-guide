0ffb6bb08e3a67007027249a571a4495ba1ad24f
Mauricio Orozco <Mauricio Orozco>
2016-12-05 15:10:03 -0500

    Added some more answers

--- drills-1.js
+++ drills-1.js
@@ -4,12 +4,14 @@
 	}
 	console.assert(addOne(10) === 11);
 	console.assert(addOne(-1) === 0);
+
 // write a function called doubleNum() that takes a number as input and returns the double of the number. 
 function doubleNum (number) {
 	return number * 2;
 }
 console.assert(doubleNum(10) === 20);
 console.assert(doubleNum(-10) === -20);
+
 // write a function called isNegative(). it should take a single, numerical input. it should output true if the number is negative, and false if the number is positive.
 function isNegative(number) {
 	if (number < 0) {
@@ -20,6 +22,7 @@ function isNegative(number) {
 }
 console.assert(isNegative(1) === false );
 console.assert(isNegative(-9999999) === true);
+
 // modify isNegative() so that it only accepts numerical input. it should return the string, "input must be a number," if bad input has been submitted.
 function isNegative2(number) {
 	if (isNaN(number) === true ) {
@@ -29,6 +32,7 @@ function isNegative2(number) {
 	}
 }
 console.assert(isNegative2(2) === true);
+
 //write a function called isEven() that tells whether a number is even or not. 
 function isEven(number) {
 	if (number % 2 === 0) {
@@ -39,6 +43,7 @@ function isEven(number) {
 }
 console.assert(isEven(6) === true);
 console.assert(isEven(999) === false);
+
 //write a function called isBig() that tells whether a number is greater than 10,000 or not.
 function isBig(number) {
 	if (number > 10000) {
@@ -49,29 +54,34 @@ function isBig(number) {
 }
 console.assert(isBig(1000000) === true);
 console.assert(isBig(9999) === false);
+
 // write a function called addPeriod(). it should take as input a string, and return a copy of that string with a period on the end. 
 function addPeriod(string) {
 	return string + '.';
 }
 console.assert(addPeriod('hello') === 'hello.');
 console.assert(addPeriod('1976') === '1976.');
+
 // write a function called sayItTwice(). it should take as input a string ('bubble') and return that string repeated twice, with a space between ('bubble bubble').
 function sayitTwice(string) {
 	return string = 'bubble' + ' bubble';
 }
 console.assert(sayitTwice('bubble') === 'bubble bubble');
+
 // write a function called firstShallBeLast(). it should add a copy of the first element of a string to the end of the string, then return the result.
 function firstShallBeLast(string) {
 	return string = string + string[0];
 }
 console.assert(firstShallBeLast('turtles') === 'turtlest');
 console.assert(firstShallBeLast('eric') === 'erice');
+
 // write a function called secondToLast(). it should return the second-to-last element of a string or array.
  arr = ['r', 'o', 'b', 'e', 'r', 't'];
  function secondToLast(array) { 
  	return array[arr.length - 2];
  }
  console.assert(secondToLast(arr) === 'r');
+
 // write a function called hasX(). it should take as input a string, and it should return true if the string contains the character 'x', and false if it does not.
 function hasX(string) {
 	if(string.includes('x')) {
@@ -82,6 +92,7 @@ function hasX(string) {
 }
 console.assert(hasX("blahxblah") === true);
 console.assert(hasX("somexwordxthatxhasxinit") === true);
+
 // write a function called hasEl(). it should take two inputs: a string and a character. it should return true if the string contains the character, and false if it does not. if you get it right, try the function on an array and a potential array element instead. does it still work?
 function hasEl(string, character) {
 	if(string = string + character) {
@@ -92,6 +103,7 @@ function hasEl(string, character) {
 }
 console.assert(hasEl("hello" + "hello") === true);
 console.assert(hasEl("cool" + "coolness") === true);
+
 // write a function called incrementEach(). 
 // it should take as input an array of numbers and return a new array that contains each number from the input array, increased by one. 
 // var arr = [1, 2, 3, 4, 5];
@@ -99,24 +111,40 @@ console.assert(hasEl("cool" + "coolness") === true);
 // 	return array[array.length++];
 // }
 // console.assert(incrementEach(arr) === [2, 3, 4, 5, 6]);
-// write a function called doubleAll(). it should take as input an array of numbers and return a new array that contains each number from the input array, doubled. 
-var arr = [1, 2, 3, 4, 5];
 
+// write a function called doubleAll(). it should take as input an array of numbers and return a new array that contains each number from the input array, doubled. 
 function doubleAll(array) {
-	return array.length * 2;
+	var newArray = [];
+	for (var i = 0; i < array.length; i++) {
+		newArray.push(array[i] * 2);
+	}
+	// console.log('newArray: ' + newArray);
+	return newArray;
 }
-console.assert(doubleAll(arr) === [2, 4, 6, 8, 10]);
+// console.log('doubleAll(newarr): ' + doubleAll(newarr))
+var newarr = [1, 2, 3, 4, 5];
+// console.log('length of newarr: ' + newarr.length)
+console.assert( (doubleAll(newarr).toString() === [2,4,6,8,10].toString()) === true);
+ 
+a = [1,2,3]            // here a and b have equivalent contents, but do not
+b = [1,2,3]            // refer to the same Array object.
+a == b                 // Thus this is false.
+console.assert((a==b) === false);
+
 // write a function called getFirstLetterCaps(). it should take as input a string, and it should return a capitalized version of the string's first character.
-// function getFirstLetterCaps(string) {
-// 	return string[0].toUpperCase();
-// }
-// console.assert(getFirstLetterCaps("mauricio") === "Mauricio");
+function getFirstLetterCaps(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+console.assert(getFirstLetterCaps("mauricio") === "Mauricio");
+console.assert(getFirstLetterCaps("Orozco") === "Orozco");
+
 // write a function called capitalizeWord(). it should take as input a string, and return a copy of that string with the first character capitalized. 
 // function capitalizeWord(string) {
-// 	return string[0].toUpperCase() + string.slice();
+// 	return string = 'mauricio' + ' Mauricio';
 // }
 
-// console.assert(capitalizeWord("mauricio") === "Mauricio");
+// console.assert(capitalizeWord('mauricio') === 'mauricio Mauricio');
+
 // write a function called capitalizeAllWords(). it should take as input an array of strings, and it should return a new array where all the words have been capitalized. HINT: use your capitalizeWord() function from above. 
 function capitalizeWord(string) {
 	return string.toUpperCase();
@@ -164,7 +192,7 @@ console.assert(reverseIt("egnaro") === "orange");
 		// {name: 'linus odoyle', age:10, email:'odoylerules@yahoo.com', hometown:'new orleans', hobby:'bullying'}
 // write a function called getKeys(). it should take as input an object, and it should return the names of all the keys (properties) of the object.
 // function getKeys(object) {
-
+//   return
 // }
 // write a function called addPropAll(). it should take three inputs: an array of objects, a property name, and a value. it should return a new array of objects, where each object has been supplemented with the key-value pair from the input. 
 // function addPropAll() {
@@ -176,3 +204,6 @@ console.assert(reverseIt("egnaro") === "orange");
 
 // }
 // write a function called getSpecificValues() that takes two inputs: an array of objects, and a property name. it should return an array containing the value stored under that property name for each object. for example, getSpecificValues(booksArray,'author') should return an array containing the author of every book object in the array. 
+// function getSpecificValues() {
+
+// }
